34f780f4e44a7b1400347ebf251732f8ee8606fa
omelyashkoea <omelyashkoea>
2021-09-23 01:28:22 +0300

    добавила вывод строк

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,9 +1,22 @@
+
 import somepackage.Other;
 import somepackage.Some;
+import somethinggreen.AnnotationForExaple;
+
+import java.lang.reflect.Method;
 
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchMethodException {
         Some someText = new Some("текст");
+
+        Class<?> cls = someText.getClass();
+
+        Method defaultDesc = AnnotationForExaple.class.getDeclaredMethod("description");
+        Object defDesc = defaultDesc.getDefaultValue();
+        System.out.println(defDesc);
+
+        String description = cls.getAnnotation(AnnotationForExaple.class).description();
+        System.out.println(description);
 
         System.out.println(someText.getSecret(false));
 
@@ -15,6 +28,10 @@ public class Test {
 
         System.out.println(someText.toString());
 
+        System.out.println(someText.getNoMoreSecrets());
 
+        System.out.println(someText.getSecret());
+
+        Other.saySomthing();
     }
 }
